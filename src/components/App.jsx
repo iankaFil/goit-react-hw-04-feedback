1a96336fc20b608145f4fe2d3b1d719f8ccda37a
iankaFil <108305015+iankaFil@users.noreply.github.com>
2023-01-24 13:26:21 -0700

    get new

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,56 +3,76 @@ import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedBackOptions';
 import { Statistics } from './Statistics/Statistic';
 import { Notification } from './Notifications/Notification';
+import { useState } from 'react';
 
-class App extends React.Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  console.log(useState(0));
+
+  // state = {
+  //   good: 0,
+  //   neutral: 0,
+  //   bad: 0,
+  // };
+
+  const countTotalFeedback = () => {
+    return good + neutral + bad;
   };
 
-  countTotalFeedback() {
-    return this.state.good + this.state.neutral + this.state.bad;
-  }
-
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const total = countTotalFeedback();
     if (total === 0) {
       return 0;
     }
-    return Math.floor((this.state.good / total) * 100);
-  }
-
-  handleLeaveFeedback = param => {
-    this.setState(prevState => {
-      return { ...prevState, [param]: prevState[param] + 1 };
-    });
+    return Math.floor((good / total) * 100);
   };
-  render() {
-    const total = this.countTotalFeedback();
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            options={Object.keys(this.state)}
-            onLeaveFeedback={this.handleLeaveFeedback}
+
+  const handleLeaveFeedback = param => {
+    // this.setState(prevState => {
+    //   return { ...prevState, [param]: prevState[param] + 1 };
+    // });
+    switch (param) {
+      case 'good':
+        setGood(good + 1);
+        break;
+      case 'neutral':
+        setNeutral(neutral + 1);
+        break;
+      case 'bad':
+        setBad(bad + 1);
+        break;
+      default:
+        break;
+    }
+  };
+
+  const total = countTotalFeedback();
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          options={Object.keys({ good, neutral, bad })}
+          onLeaveFeedback={handleLeaveFeedback}
+        />
+      </Section>
+      {total === 0 ? (
+        <Notification message="No feedback given" />
+      ) : (
+        <Section title="Statistics">
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            percentage={countPositiveFeedbackPercentage()}
           />
         </Section>
-        {total === 0 ? (
-          <Notification message="No feedback given" />
-        ) : (
-          <Section title="Statistics">
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={total}
-              percentage={this.countPositiveFeedbackPercentage()}
-            />
-          </Section>
-        )}
-      </>
-    );
-  }
-}
+      )}
+    </>
+  );
+};
+
 export { App };
--- src/components/Statistics/Statistic.jsx
+++ src/components/Statistics/Statistic.jsx
@@ -4,11 +4,11 @@ import PropTypes from 'prop-types';
 
 export const Statistics = props => (
   <>
-    <p className={css.variant}>Good:{props.good}</p>
-    <p className={css.variant}>Neutral:{props.neutral}</p>
-    <p className={css.variant}>Bad:{props.bad}</p>
-    <p className={css.variant}>Total:{props.total}</p>
-    <p className={css.variant}>Positive feedback:{props.percentage}%</p>
+    <p className={css.variant}>Good: {props.good}</p>
+    <p className={css.variant}>Neutral: {props.neutral}</p>
+    <p className={css.variant}>Bad: {props.bad}</p>
+    <p className={css.variant}>Total: {props.total}</p>
+    <p className={css.variant}>Positive feedback: {props.percentage}%</p>
   </>
 );
 Statistics.propTypes = {
